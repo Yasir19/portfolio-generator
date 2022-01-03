12797d0d117bc39e1a4f6d78e873a859c9ac96a2
yasir19 <yasir.habboo@gmail.com>
2022-01-02 18:26:56 -0600

    generate HTML page

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
+const fs = require('fs');
+const generatePage = require('./src/page-template');
 const profileDataArgs = process.argv.slice(2);
-const printProfileData = profileDataArr => {
-    profileDataArr.forEach(profileItem => console.log(profileItem));
-};
-printProfileData(profileDataArgs);
+const [name,github] = profileDataArgs;
+fs.writeFile('index.html',generatePage(name, github), err => {
+    if (err) throw err;
+    console.log('Portfolio complete! Check out index.html to see the output!');
+});
