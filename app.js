a39dfdb5a16eef0f7851c4359036761baa44c472
yasir19 <yasir.habboo@gmail.com>
2022-01-04 17:02:46 -0600

    working app

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const fs = require('fs');
+const {writeFile, copyFile} = require ('./utils/generate-site');
 const inquirer = require('inquirer');
 const generatePage = require('./src/page-template');
 const promptUser =()=> {
@@ -149,12 +149,22 @@ return inquirer.prompt([
 };
 promptUser().then(promptProjects)
 .then(portfolioData => {
-    const pageHTML = generatePage(portfolioData);
-    fs.writeFile('index.html',pageHTML, err => {
-    if (err) throw err;
-    console.log('Portfolio complete! Check out index.html to see the output!');
+    return generatePage(portfolioData);
+})
+.then(pageHTML => {
+    return writeFile(pageHTML);
+})
+.then(writeFileResponse =>{
+    console.log(writeFileResponse);
+    return copyFile();
+})
+.then(copyFileResponse => {
+    console.log(copyFileResponse);
+})
+.catch(err =>{
+    console.log(err)
 });
-});
+
 
 
 
